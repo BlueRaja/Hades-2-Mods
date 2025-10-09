80541a1d0a5150e95546eb633a50a429700bc93a
BlueRaja <BlueRaja.admin@gmail.com>
2025-10-08 23:03:05 -0700

    Added Olympus-Third-Hammer incantation

---
+++ FavorOfTheGods/incantations/olympus_third_hammer.lua
@@ -0,0 +1,83 @@
+-- Logic
+local function enableOlympusThirdHammer()
+    -- Allow early hammer upgrade if you've found less than 2 hammers
+    for i, requirement in ipairs(NamedRequirementsData.HammerLootRequirements) do
+        if requirement.PathFalse and 
+           requirement.PathFalse[1] == "CurrentRun" and 
+           requirement.PathFalse[2] == "LootTypeHistory" and 
+           requirement.PathFalse[3] == "WeaponUpgrade" then
+            NamedRequirementsData.HammerLootRequirements[i] = {
+                Path = { "CurrentRun", "LootTypeHistory", "WeaponUpgrade" },
+                Comparison = "<",
+                Value = 2,
+            }
+            break
+        end
+    end
+    
+    -- Allow late hammer upgrade if you've found less than 3 hammers
+    for i, requirement in ipairs(NamedRequirementsData.LateHammerLootRequirements) do
+        if requirement.Path and 
+           requirement.Path[1] == "CurrentRun" and 
+           requirement.Path[2] == "LootTypeHistory" and 
+           requirement.Path[3] == "WeaponUpgrade" and
+            NamedRequirementsData.LateHammerLootRequirements[i] = {
+                Path = { "CurrentRun", "LootTypeHistory", "WeaponUpgrade" },
+                Comparison = "<",
+                Value = 3,
+            }
+            break
+        end
+    end
+end
+
+-- Incantation
+Incantations.addIncantation({
+    Id = "BlueRaja-Olympus-Third-Hammer",
+    Name = "Favor of Olympus",
+    Description = "Allows you to sometimes find a third Daedalus Hammer.",
+    FlavorText = "The collective blessing of Olympus grants you access to the master craftsman's tools beyond their usual limits, allowing for unprecedented weapon enhancement.",
+    OnEnabled = enableOlympusThirdHammer,
+    WorldUpgradeData = {
+        InheritFrom = { "DefaultMajorItem", "DefaultCriticalItem" },
+        Icon = "GUI\\Screens\\AwardMenu\\KeepsakeMaxGift\\KeepsakeMaxGift_small\\Zeus",
+        Cost = {
+            MixerIBoss = 2,             -- Zodiac Sand
+            MixerQBoss = 2,             -- Void Lens
+            WeaponPointsRare = 8,       -- Nightmare
+            Mixer5Common = 8,           -- Star Dust
+            MetaCardPointsCommon = 250, -- Ash
+        },
+        GameStateRequirements = {
+            {   -- Three hearts with all gods on Olympus
+                Path = { "GameState", "TextLinesRecord" },
+                CountOf = {
+                    "ZeusGift03", "HeraGift03", "PoseidonGift03", "DemeterGift03", 
+                    "AphroditeGift03", "HephaestusGift03", "HestiaGift03", "AthenaGift03",
+                    "AresGift03", "ArtemisGift03", "ApolloGift03", "HermesGift03", "DionysusGift03"
+                },
+                Comparison = ">=",
+                Value = 13,
+            },
+            {   -- Have gotten at least one Zodiac Sand before
+                Path = { "GameState", "LifetimeResourcesGained", "MixerIBoss" },
+                Comparison = ">=",
+                Value = 1,
+            },
+            {   -- Have gotten at least one Void Lens before
+                Path = { "GameState", "LifetimeResourcesGained", "MixerQBoss" },
+                Comparison = ">=",
+                Value = 1,
+            },
+        },
+        IncantationVoiceLines = {
+            {
+                PreLineWait = 0.3,
+                { Cue = "/VO/Melinoe_0198", Text = "Olympus, I accept this message!" }
+            },
+        },
+        RevealReactionVoiceLines = {
+            { GlobalVoiceLines = "CauldronReactionVoiceLines" },
+        },
+    }
+})
--- FavorOfTheGods/ready.lua
+++ FavorOfTheGods/ready.lua
@@ -18,8 +18,21 @@ function roundRandomly(roundMe)
     end
 end
 
+function isDuringRun()
+    return CurrentHubRoom == nil
+end
+
+import "incantations/aphrodite_nectar_refund.lua"
+import "incantations/arachne_armor_boost.lua"
+import "incantations/chaos_free_reroll.lua"
+import "incantations/charon_gold_drops.lua"
 import "incantations/demeter_more_plants.lua"
+import "incantations/dora_shade_damage.lua"
+import "incantations/echo_shop_respawn.lua"
+import "incantations/eris_sell_rubbish.lua"
+import "incantations/god_encounter_boosts.lua"
 import "incantations/hephaestus_more_ore.lua"
 import "incantations/narcissus_max_hp.lua"
-import "incantations/dora_shade_damage.lua"
-import "incantations/aphrodite_nectar_refund.lua"
+import "incantations/olympus_third_hammer.lua"
+import "incantations/poseidon_rare_fish.lua"
+import "incantations/selene_card_upgrades.lua"
