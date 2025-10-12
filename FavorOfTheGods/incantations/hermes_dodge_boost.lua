1456d9ecc91f85232b0f85de555c4cc0b25c1f7d
BlueRaja <BlueRaja.admin@gmail.com>
2025-10-11 22:48:05 -0700

    Added Hermes-Dodge-Boost incantation

---
+++ FavorOfTheGods/incantations/hermes_dodge_boost.lua
@@ -0,0 +1,71 @@
+-- Logic
+local function enableHermesDodgeBoost()
+    OverwriteTableKeys(TraitData, {
+        BlueRajaHermesDodgeBoost = {
+            InheritFrom = { "BaseTrait" },
+            PropertyChanges = {
+                {
+                    LifeProperty = "DodgeChance",
+                    ChangeValue = 0.05,
+                    ChangeType = "Add",
+                    DataValue = false,
+                },
+            },
+            StatLines = {
+                "DodgeChanceStatDisplay",
+            },
+            ExtractValues = {
+                {
+                    Key = "ChangeValue",
+                    ExtractAs = "TooltipDodgeBonus",
+                    Format = "Percent",
+                },
+            },
+        }
+    })
+end
+
+ModUtil.mod.Path.Wrap("StartNewRun", function(base, prevRun, args)
+    local result = base(prevRun, args)
+    
+    if Incantations.isIncantationEnabled("BlueRaja-Hermes-Dodge-Boost") then
+        AddTraitToHero({ TraitData = TraitData.BlueRajaHermesDodgeBoost })
+        printMsg("[Hermes] Added dodge chance")
+    end
+    
+    return result
+end)
+
+-- Incantation
+Incantations.addIncantation({
+    Id = "BlueRaja-Hermes-Dodge-Boost",
+    Name = "Favor of Hermes",
+    Description = "Increases dodge chance by 5%.",
+    FlavorText = "Hermes's blessing grants you the swiftness of the wind, allowing you to evade attacks with supernatural grace and agility.",
+    OnEnabled = enableHermesDodgeBoost,
+    WorldUpgradeData = {
+        InheritFrom = { "DefaultHubItem", "DefaultCriticalItem" },
+        Icon = "GUI\\Screens\\AwardMenu\\KeepsakeMaxGift\\KeepsakeMaxGift_small\\Hermes",
+        Cost = {
+            MixerPBoss = 1,         -- Eagle's Feather
+            OreNBronze = 3,         -- Bronze
+            GiftPointsRare = 3,     -- Bath Salts
+        },
+        GameStateRequirements = {
+            {   -- Four hearts with Hermes
+                PathTrue = { "GameState", "TextLinesRecord", "HermesGift04" },
+            },
+            {   -- Have gotten at least one Bath Salt before
+                Path = { "GameState", "LifetimeResourcesGained", "GiftPointsRare" },
+                Comparison = ">=",
+                Value = 1,
+            },
+        },
+        IncantationVoiceLines = {
+            {
+                PreLineWait = 0.3,
+                { Cue = "/VO/Melinoe_1363", Text = "Rush delivery on this." }
+            },
+        },
+    }
+})
